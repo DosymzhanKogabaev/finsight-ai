48470c63a20af40d2d68e9a5ae9d6be552a24427
dosik <dkogabayev@mail.ru>
2025-11-28 19:05:42 +0500

    fix(auth): response schema for register api

--- src-backend/apps/auth/api/public/publicRegister.ts
+++ src-backend/apps/auth/api/public/publicRegister.ts
@@ -20,7 +20,7 @@ const RESPONSE_SCHEMA = z.object({
 export class PublicRegisterAPI extends OpenAPIRoute {
 	schema = {
 		request: { body: { content: { 'application/json': { schema: REQUEST_BODY_SCHEMA } } } },
-		response: { '200': { content: { 'application/json': { schema: RESPONSE_SCHEMA } } } },
+		responses: { '200': { content: { 'application/json': { schema: RESPONSE_SCHEMA } } } },
 	} as any;
 
 	async handle(_request: IRequest, env: Env, _ctx: ExecutionContext) {
